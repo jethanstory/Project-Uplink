043aa2d35f9939713e67d343a59def917c770dff
jethanstory <jstory@csu.fullerton.edu>
2022-10-07 13:17:31 -0700

    Basic menu for the mascot implemented

--- Project Uplink/Assets/Scripts/GameOEvents.cs
+++ Project Uplink/Assets/Scripts/GameOEvents.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class GameOEvents : MonoBehaviour
 {
+
+    public GameObject defaultMascot;
+    public GameObject mascotMenu;
     // public int counter = 0;
 
     // void awake() 
@@ -33,5 +36,21 @@ public class GameOEvents : MonoBehaviour
         //SceneManager.LoadScene("3DLevel_2"); //Level_2 //3DLevel_2
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         //if (counter)
+    }
+    public void mascotMenuTrigger()
+    {
+        //defaultCanvas.SetActive(false);
+        mascotMenu.SetActive(true);
+        defaultMascot.SetActive(false);
+    }
+    public void mascotMenuTriggerExit()
+    {
+        //if (mascotMenu.activeSelf) 
+        //{
+            defaultMascot.SetActive(true);
+            mascotMenu.SetActive(false);
+        //}
+        
+        //mascotCanvas.SetActive(true);
     }
 }
