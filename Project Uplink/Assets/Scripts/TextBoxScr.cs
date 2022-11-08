69855d18cb9af93465d83d6bc84027d4abb8a5d4
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2022-11-07 23:10:27 -0800

    added comments to the text box script

--- Project Uplink/Assets/Scripts/TextBoxScr.cs
+++ Project Uplink/Assets/Scripts/TextBoxScr.cs
@@ -17,27 +17,44 @@ public class TextBoxScr : MonoBehaviour
 
     public void Start()
     {
+        //get the text of the paragraph
         myParagraphText = myParagraph.GetComponent<Text>();
+
+        //get the input field
         myInputField = GetComponent<InputField>();
+
+        //set the chars array
         myChars = new char[myParagraphText.text.Length];
+
+        //get the max number of chars in the paragraph
         maxNum = myParagraphText.text.Length;
+
+        //get the text in the textbox
         myText = transform.GetChild(1).GetComponent<Text>();
+
+        //adds the chars from the paragraph to the chars array
         GetLetters();
     }
 
     public void Update()
     {
+        //check if the player has pressed a key
         if (Input.anyKey)
         {
+            //set this text to equal the text in its current state
             myText = transform.GetChild(transform.childCount - 1).GetComponent<Text>();
+
+            //check all the chars to see if its right
             CheckLetters();
         }
 
+        //check if the first wrong char has been deleted
         if (posWrong > myText.text.Length)
         {
             posWrong = -1;
         }
 
+        //checks if the paragraph is complete
         if (correctNum >= maxNum)
         {
             FindObjectOfType<InfosqueakScr>().progressNum++;
@@ -48,6 +65,7 @@ public class TextBoxScr : MonoBehaviour
 
     private void GetLetters()
     {
+        //takes chars from paragraph and puts it into the chars array
         for (int i = 0; i < myParagraphText.text.Length; i++)
         {
             char currChar = myParagraphText.text[i];
@@ -57,21 +75,29 @@ public class TextBoxScr : MonoBehaviour
 
     private void CheckLetters()
     {
+        //checks for any incorrect chars
         for (int i = 0; i < myText.text.Length; i++)
         {
             char currChar = myText.text[i];
 
+            //checks if the current char is incorrect
             if (currChar != myChars[i])
             {
+                //sets the first position of the incorrect char to equal the position of the current char
                 if (posWrong == -1)
                 {
                     posWrong = i;
                 }
+                //sets the textbox to red to indicate that it is wrong
                 myInputField.image.color = Color.red;
+                //exits out of the method
                 return;
             }
         }
+        //every letter is currently correct
+        //set the number of correct chars to equal the current length of the text in the textbox
         correctNum = myText.text.Length;
+        //sets the textbox to white to indicate that it is correct
         myInputField.image.color = Color.white;
     }
 }
