c03b97e7ccff65fa537a12d4612ff1f378adb985
jethanstory <jstory@csu.fullerton.edu>
2022-11-26 15:57:39 -0800

    Added some minor changes to the modern news page

---
+++ Project Uplink/Assets/Scripts/CurrentDateTimeScri.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentDateTimeScri : MonoBehaviour
+{
+    // public static DateTime theTime = DateTime.Now; //the time
+    // [SerializeField] private string date = theTime.ToString("yyyy-MM-dd\\Z"); //convert time and date to string
+    // [SerializeField] private string time = theTime.ToString("HH:mm:ss");
+    // [SerializeField] private string dateTime = theTime.ToString("yyyy-MM-dd\\THH:mm:ss\\Z");
+    
+    public GameObject textDisplay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string dateTime = System.DateTime.UtcNow.ToLocalTime().ToString("dddd M/d/yy  hh:mm tt");
+        //string dateTime = System.DateTime.UtcNow.ToLocalTime().ToString("dddd M/d/yy");
+        //userInput = searchBar.GetComponent<InputField>().text;
+        textDisplay.GetComponent<Text>().text = dateTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
