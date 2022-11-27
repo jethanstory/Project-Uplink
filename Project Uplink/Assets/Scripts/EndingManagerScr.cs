764db459b7152c3387a3eea821aa052771f57e80
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2022-11-26 18:32:25 -0800

    Mostly finished the drop box ending

---
+++ Project Uplink/Assets/Scripts/EndingManagerScr.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingManagerScr : MonoBehaviour
+{
+    public int endingNum;
+    public GameObject cutsceneMouse;
+
+    public void Start()
+    {
+        Scene myScene = SceneManager.GetActiveScene();
+
+        if (myScene.name == "Dropbox_Ending")
+        {
+            //Drag box ending
+
+            //make cursor invisible
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            //create cutscene mouse
+            GameObject myMouse = Instantiate(cutsceneMouse, Vector3.zero, Quaternion.identity);
+            myMouse.GetComponent<CutsceneMouseScr>().endingNum = 0;
+        }
+    }
+
+    public void StartRollCredits()
+    {
+        Invoke("RollCredits", 10f);
+    }
+
+    private void RollCredits()
+    {
+        SceneManager.LoadScene("Credits_Scene");
+    }
+}
