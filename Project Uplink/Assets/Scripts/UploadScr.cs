5678e602d7c6646df1f46456d9b57ff390ed4d70
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2022-12-02 00:59:55 -0800

    added blue screen

--- Project Uplink/Assets/Scripts/UploadScr.cs
+++ Project Uplink/Assets/Scripts/UploadScr.cs
@@ -1,29 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NoUninstallScr : MonoBehaviour
+public class UploadScr : MonoBehaviour
 {
     public Button myButton;
-    public GameObject uninstallScreen1;
     public GameObject blackScreen;
     private GameObject currBlackScreen;
 
-    public void StartNeutralEnding()
+    public void BeginEnding()
     {
-        FindObjectOfType<PresqueakScr>().cutsceneNum++;
-
         currBlackScreen = Instantiate(blackScreen, Vector3.zero, Quaternion.identity);
         currBlackScreen.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
 
-        Invoke("GoToEnding", 10f);
-    }
-
-    public void GoToEnding()
-    {
-        //SceneManager.LoadScene("Dropbox_Ending");
-        SceneManager.LoadScene("Dropbox_EndingSimple");
+        FindObjectOfType<EndingManagerScr>().StartRollCredits();
     }
 }
